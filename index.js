b0644bb7880a434ee81c49057a713b9f51ef5946
Ben Wiley <therealbenwiley@gmail.com>
2016-12-08 01:54:41 -0500

    PropTypes included for integer, color, datetime, uuid.

---
+++ index.js
@@ -0,0 +1,11 @@
+var integer = require('./lib/integer');
+var color = require('./lib/color');
+var datetime = require('./lib/datetime');
+var uuid = require('./lib/uuid');
+
+module.exports = {
+  integer: integer,
+  color: color,
+  datetime, datetime,
+  uuid: uuid
+};
